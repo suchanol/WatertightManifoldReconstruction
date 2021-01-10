407744b0b0617a1cf73a0791a584604202772478
Oliver Suchan <oliver.suchan@student.kit.edu>
2021-01-10 14:18:41 +0100

    Added Grid class.

---
+++ Grid.py
@@ -0,0 +1,26 @@
+class Grid:
+
+    def make_grid_from_file(self, file, resolution):
+        pass
+
+    def make_grid(self, points, resolution):
+        pass
+
+    def get_voxels(self):
+        pass
+
+    def get_voxel(self, p):
+        pass
+
+    def insert_point(self, p):
+        pass
+
+    # (up, down, front, back, left, right)
+    def get_neighbors(self, v):
+        pass
+
+    def is_valid(self, v):
+        pass
+
+    def get_bounds(self):
+        pass
